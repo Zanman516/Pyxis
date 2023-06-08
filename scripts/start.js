c4f2eebc4559d39146be0295fa6132a26f26a031
Zanman516 <129312841+Zanman516@users.noreply.github.com>
2023-06-08 12:47:06 -0400

    Update start.js

--- scripts/start.js
+++ scripts/start.js
@@ -35,7 +35,7 @@ const rammerheadScopes = [
 
 const rammerheadSession = /^\/[a-z0-9]{32}/;
 
-console.log(`${chalk.cyan('Begging StartUp Phase!')}\n`);
+console.log(`${chalk.cyan('Beggining StartUp Phase!')}\n`);
 
 const app = express();
 
@@ -128,7 +128,7 @@ while (true) {
 		);
 
 		console.log(
-			`You can now view ${chalk.bold('website-aio')} in the browser.`
+			`You can now view ${chalk.bold('The Proxy')} in the browser.`
 		);
 
 		console.log('');
